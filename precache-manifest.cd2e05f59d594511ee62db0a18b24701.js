04437428f34f0ca5cce945a1b77ca55c751c0add
Cosmina Palade <cosmina.palade@gmail.com>
2020-04-02 13:05:59 +0300

    Updates

---
+++ precache-manifest.cd2e05f59d594511ee62db0a18b24701.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dd001a98f024dc699eded663c4d6c77",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b30de238e8408db8c715",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "59fa3398e79628ef96dc",
+    "url": "/static/js/2.8e1b5080.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8e1b5080.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b30de238e8408db8c715",
+    "url": "/static/js/main.7b63ebd9.chunk.js"
+  },
+  {
+    "revision": "f7a6a9ac051315865378",
+    "url": "/static/js/runtime-main.d72cd338.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
